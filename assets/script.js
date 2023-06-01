38701d2b43b3fcf8c42f3429e117ef16b4727008
Rajni bala <balatamna16@gmail.com>
2023-05-31 23:42:16 -0400

    add event listener on search button

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+// my APIKey is "b4ed94adc674507878b0aeb7f93e988b";
+$(document).ready(function(){
+  //event listener on search button
+$("#searchbtn").on("click",function(){
+  //get value in input search value
+  var searchCity = $("#search-value").val();
+  $("search-value").val(" ");
+  weatherDashboard(searchCity);
+  weatherForecast(searchCity);
+});
+//The keypress event is fired when a key that produces a character value is pressed down.
+$("#searchbtn").keypress(function(event){
+  var keycode = (event.keycode ? event.keycode : event.which);
+  if(keycode === 13)
+  {
+    weatherDashboard(searchCity);
+    weatherForecast(searchCity);
+  }
+})
